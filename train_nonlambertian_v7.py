d7ffbc49b0171d4d13354cb4e7651cfb05f1b323
Leonardo-Zhou <2974519865@qq.com>
2025-08-30 14:53:46 +0800

    v7

--- train_nonlambertian_v7.py
+++ train_nonlambertian_v7.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-from nonlambertian_trainer_v7 import NonLambertianTrainer
+from nonlambertian_trainer_v7 import NonLambertianTrainerV7
 from nonlambertian_options_v7 import NonLambertianOptions
 
 
@@ -8,5 +8,5 @@ options = NonLambertianOptions()
 opts = options.parse()
 
 if __name__ == "__main__":
-    trainer = NonLambertianTrainer(opts)
+    trainer = NonLambertianTrainerV7(opts)
     trainer.train()
